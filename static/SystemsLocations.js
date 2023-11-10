cf156c4e08bac2c71ee5b18fc856518222bb8192
gsmithw91 <107428344+gsmithw91@users.noreply.github.com>
2023-11-10 15:43:57 -0600

    commit

--- static/SystemsLocations.js
+++ static/SystemsLocations.js
@@ -6,41 +6,36 @@ window.onload = function () {
       data.forEach((system) => {
         const button = document.createElement("button");
         button.innerText = system.SystemName;
-        button.addEventListener("click", function () {
-          // Call the function to load charges for the selected system
-          loadChargesForSystem(system.SystemID);
-        });
+        button.addEventListener("click", () =>
+          loadChargesForSystem(system.SystemID)
+        );
         systemsContainer.appendChild(button);
       });
     })
-    .catch((error) => {
-      console.error("Error fetching systems:", error);
-    });
+    .catch((error) => console.error("Error fetching systems:", error));
 };
+
 function loadChargesForSystem(systemId) {
-  const chargesContainer = document.getElementById("charges-container");
   const chargesTable = document.getElementById("charges-table");
-  chargesTable.innerHTML = ""; // Clear the table
+  chargesTable.innerHTML = ""; // Clear previous table data
 
   fetch(`/api/charges/system/${systemId}`)
     .then((response) => response.json())
     .then((response) => {
       const { data: chargeData, columns: columnOrder } = response;
-      if (chargeData && chargeData.length > 0 && columnOrder) {
-        // Create thead and tbody elements
-        const thead = chargesTable.createTHead();
-        const tbody = document.createElement("tbody");
-        chargesTable.appendChild(tbody);
-
-        // Create table headers using the column order from the response
+      if (chargeData && chargeData.length > 0) {
+        // Create table headers
+        const thead = document.createElement("thead");
         const headerRow = thead.insertRow();
         columnOrder.forEach((header) => {
           const th = document.createElement("th");
-          th.textContent = header; // Assuming column names are already formatted
+          th.textContent = header;
           headerRow.appendChild(th);
         });
+        chargesTable.appendChild(thead);
 
-        // Create table rows using the column order from the response
+        // Create table body
+        const tbody = document.createElement("tbody");
         chargeData.forEach((item) => {
           const row = tbody.insertRow();
           columnOrder.forEach((header) => {
@@ -48,15 +43,11 @@ function loadChargesForSystem(systemId) {
             cell.textContent = item[header];
           });
         });
+        chargesTable.appendChild(tbody);
       } else {
-        // If no data or no column order, clear the table and display a message
-        chargesTable.innerHTML = "";
-        const noDataMessage = document.createElement("p");
-        noDataMessage.textContent = "No charge data available for this system.";
-        chargesContainer.appendChild(noDataMessage);
+        // Handle no data
+        console.log("No charge data available for this system.");
       }
     })
-    .catch((error) => {
-      console.error("Error fetching charge data:", error);
-    });
+    .catch((error) => console.error("Error fetching charge data:", error));
 }
