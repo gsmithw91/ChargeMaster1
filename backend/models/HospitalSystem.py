94aee5d91ece6c0ea0f1f33c887ef26e83bc2331
gsmithw91 <107428344+gsmithw91@users.noreply.github.com>
2023-11-16 13:10:41 -0600

    fixed nulls in models

--- backend/models/HospitalSystem.py
+++ backend/models/HospitalSystem.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, ValidationError, validator
 from typing import Optional
 
 class HospitalSystem(BaseModel):
@@ -7,3 +7,7 @@ class HospitalSystem(BaseModel):
     Headquarters : Optional [str] = None
     FoundedYear : Optional [int] = None
     Description : Optional [str] = None
+    
+    @validator('FoundedYear', pre=True, allow_reuse=True)
+    def parse_founded_year(cls, v):
+        return None if v in ['NULL', 'null', 'Null'] else v
