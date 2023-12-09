48ff7e8a98e4b7f7c2b1d69d917eecd0d0252305
gsmithw91 <107428344+gsmithw91@users.noreply.github.com>
2023-12-09 11:53:49 -0600

    nu

--- __init__.py
+++ __init__.py
@@ -13,4 +13,4 @@ app.register_blueprint(errors)
 app.register_blueprint(web)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=False)  # Run on localhost and port 5000
+    app.run(host='0.0.0.0', port=5000, debug=False)  # Run on localhost and port 5000
