d8ae1cfbf7a6c81c03b74c88a2d5bfdf5386ffe6
gsmithw91 <107428344+gsmithw91@users.noreply.github.com>
2023-12-15 07:41:32 -0600

    fixed custom

--- logs/custom_logger.py
+++ logs/custom_logger.py
@@ -1,45 +1,40 @@
 import logging
 import os
 from logging.handlers import TimedRotatingFileHandler
-from datetime import datetime
 
+# Base directory for logs - same level as custom_logger.py
+base_logs_dir = os.path.dirname(__file__)
 
-base_logs_dir = os.path.join(os.path.dirname(__file__), 'logs')
+def create_logger(name, level, log_subdir, log_filename, when='midnight', interval=1, backup_count=30):
+    # Path for the log subdirectory
+    log_dir_path = os.path.join(base_logs_dir, log_subdir)
+    os.makedirs(log_dir_path, exist_ok=True)
 
-def create_logger(name, level, log_filename, when='midnight', interval=1, backup_count=30):
-    # Ensure the base log directory exists
-    os.makedirs(base_logs_dir, exist_ok=True)
-    
+    # Full path for the log file
+    log_file_path = os.path.join(log_dir_path, log_filename)
+
+    # Create the logger
     logger = logging.getLogger(name)
     logger.setLevel(level)
-    
-    # Log file path
-    log_file_path = os.path.join(base_logs_dir, log_filename)
-    
-    # Log file handler with rotation
-    file_handler = TimedRotatingFileHandler(
-        log_file_path, when=when, interval=interval, backupCount=backup_count
-    )
+
+    # Create the file handler for logging
+    file_handler = TimedRotatingFileHandler(log_file_path, when=when, interval=interval, backupCount=backup_count)
     file_handler.setLevel(level)
-    
-    # Stream handler for console output
-    stream_handler = logging.StreamHandler()
-    stream_handler.setLevel(logging.DEBUG if os.getenv('DEBUG', 'False') == 'True' else logging.INFO)
-    
-    # Formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     file_handler.setFormatter(formatter)
-    stream_handler.setFormatter(formatter)
     
+    # Optionally add a stream handler to output logs to console
+    stream_handler = logging.StreamHandler()
+    stream_handler.setLevel(level)
+    stream_handler.setFormatter(formatter)
+
     # Add handlers to the logger
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
-    
+
     return logger
 
-
-
-
-api_logger = create_logger('api_logger', logging.INFO, os.path.join('api', 'api_activity.log'))
-web_logger = create_logger('web_logger', logging.INFO, os.path.join('web', 'web_activity.log'))
-error_logger = create_logger('error_logger', logging.ERROR, os.path.join('error', 'error.log'))
+# Create loggers
+api_logger = create_logger('api_logger', logging.INFO, 'api', 'api_activity.log')
+web_logger = create_logger('web_logger', logging.INFO, 'web', 'web_activity.log')
+error_logger = create_logger('error_logger', logging.ERROR, 'error', 'error.log')
