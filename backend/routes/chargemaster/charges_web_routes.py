97e15ea8faa80c76eaffeb880bd85afaf102bd60
gsmithw91 <107428344+gsmithw91@users.noreply.github.com>
2024-01-08 22:52:37 -0600

    added single charge search

--- backend/routes/chargemaster/charges_web_routes.py
+++ backend/routes/chargemaster/charges_web_routes.py
@@ -16,22 +16,22 @@ def index():
     return render_template('index.html')
 
 
-@web.route('/chargemaster')
-def chargemaster():
-    web_logger.info(f"Accessed the home page with method {request.method} and headers {request.headers}")
-    try:
-        # Use the updated get_filtered_data function without system_id and location_id
-        systems_data = get_filtered_data("HospitalSystem")
-        systems = [HospitalSystem(**system).dict() for system in systems_data]
-        return render_template('chargemaster.html', systems=systems)
-    except ValidationError as e:
-        web_logger.error(f"Data validation error for systems: {e}")
-        # Handle the error as you see fit (e.g., show a custom error page)
-        return render_template('error.html', error="Invalid data format"), 500
-    except Exception as e:
-        web_logger.error(f"An error occurred while accessing the home page: {e}")
-        # Handle the error as you see fit (e.g., show a custom error page)
-        return render_template('error.html', error="An error occurred"), 500
+# @web.route('/chargemaster')
+# def chargemaster():
+#     web_logger.info(f"Accessed the home page with method {request.method} and headers {request.headers}")
+#     try:
+#         # Use the updated get_filtered_data function without system_id and location_id
+#         systems_data = get_filtered_data("HospitalSystem")
+#         systems = [HospitalSystem(**system).dict() for system in systems_data]
+#         return render_template('chargemaster.html', systems=systems)
+#     except ValidationError as e:
+#         web_logger.error(f"Data validation error for systems: {e}")
+#         # Handle the error as you see fit (e.g., show a custom error page)
+#         return render_template('error.html', error="Invalid data format"), 500
+#     except Exception as e:
+#         web_logger.error(f"An error occurred while accessing the home page: {e}")
+#         # Handle the error as you see fit (e.g., show a custom error page)
+#         return render_template('error.html', error="An error occurred"), 500
 
 
 @web.route('/display-chargesheet', methods=['GET'])
