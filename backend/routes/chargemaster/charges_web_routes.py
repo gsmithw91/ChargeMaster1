4859406527fb3352b6ebd4eed27d8759adf13105
gsmithw91 <107428344+gsmithw91@users.noreply.github.com>
2024-01-10 21:54:27 -0600

    pushing new feature Login,needs db_helpers

--- backend/routes/chargemaster/charges_web_routes.py
+++ backend/routes/chargemaster/charges_web_routes.py
@@ -1,4 +1,3 @@
-# backend/routes/web_routes.py
 from flask import Blueprint, render_template, request, json, session, jsonify, send_from_directory, redirect
 from backend.database.db_helpers import get_filtered_data
 from backend.models.HospitalSystem import HospitalSystem
@@ -10,29 +9,17 @@ from logs.custom_logger import web_logger
 
 web = Blueprint('web', __name__)
 
-
+# Serve the React app's main entry point
 @web.route('/')
 def index():
-    return render_template('index.html')
+    return send_from_directory('static', 'index.html')
+
+# Serve static files (CSS, JS, images, etc.)
+@web.route('/static/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('static', filename)
+
+# Redirect all non-API requests to the index.html for React Router to handle
 @web.route('/<path:path>', methods=['GET'])
 def catch_all(path):
-    return redirect('/')
-
-# @web.route('/chargemaster')
-# def chargemaster():
-#     web_logger.info(f"Accessed the home page with method {request.method} and headers {request.headers}")
-#     try:
-#         # Use the updated get_filtered_data function without system_id and location_id
-#         systems_data = get_filtered_data("HospitalSystem")
-#         systems = [HospitalSystem(**system).dict() for system in systems_data]
-#         return render_template('chargemaster.html', systems=systems)
-#     except ValidationError as e:
-#         web_logger.error(f"Data validation error for systems: {e}")
-#         # Handle the error as you see fit (e.g., show a custom error page)
-#         return render_template('error.html', error="Invalid data format"), 500
-#     except Exception as e:
-#         web_logger.error(f"An error occurred while accessing the home page: {e}")
-#         # Handle the error as you see fit (e.g., show a custom error page)
-#         return render_template('error.html', error="An error occurred"), 500
-
-
+    return send_from_directory('static', 'index.html')
