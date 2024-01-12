19e2b32d251d5e7d50084864ed976d2d7a78d3b5
gsmithw91 <107428344+gsmithw91@users.noreply.github.com>
2024-01-12 02:50:37 -0600

    reigstered login blueprint

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-# main.py
 from flask import Flask, jsonify
 from flask_cors import CORS  # Import the CORS module
 from backend.routes.chargemaster.charges_api_routes import api
@@ -7,21 +6,22 @@ from backend.routes.chargemaster.charges_web_routes import web
 from backend.routes.chargemaster.charges_react_api_routes import react_api
 from backend.routes.eligbilitytool.elig_react_api_routes import elig_api
 from backend.routes.db_admin.db_admin_routes import db_admin_api
+from backend.routes.login.login_routes import login_api  # Import the login_api Blueprint
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = 'fappie'
 
 # Enable CORS for your app
-CORS(app, resources={r"/react/*": {"origins": "http://localhost:3000"}})  # Replace with the appropriate route
+CORS(app, resources={r"/react/*": {"origins": "http://localhost:3000"}})  # Adjust as needed
 
-# Register the blueprint
+# Register the blueprints
 app.register_blueprint(api)
 app.register_blueprint(errors)
 app.register_blueprint(web)
 app.register_blueprint(react_api)
 app.register_blueprint(elig_api)
 app.register_blueprint(db_admin_api)
-
+app.register_blueprint(login_api)  # Register the login_api Blueprint
 
 @app.route('/json-file', methods=['GET'])
 def get_json_file():
@@ -30,7 +30,6 @@ def get_json_file():
         return app.send_static_file('smithtech_openapi_with_insights.json')
     except Exception as e:
         return jsonify({"error": "An error occurred while serving the JSON file"}), 500
-
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=5000, debug=True)
