88ff687f0bd29997d4393bb593775c1243eaba42
gsmithw91 <107428344+gsmithw91@users.noreply.github.com>
2024-01-29 10:41:22 -0600

    Revert "changed CORS"
    This reverts commit d24aa1fc7d0ddd1dc715d97a0d618568edf6fb9c.

--- main.py
+++ main.py
@@ -14,8 +14,7 @@ from backend.routes.chargesheet.chargesheet_routes import chargesheet_api
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = 'fappie'
 # Enable CORS for your app
-CORS(app)
-
+CORS(app, resources={r"/auth/*": {"origins": "http://localhost:3000"}})
 app.config['JWT_SECRET_KEY'] = 'fappie'  # Change this to a random secret key
 jwt = JWTManager(app)
 
